749a4491e0bcddd925a9cef81f681b23196db056
MiguelRaposo49456 <A49456@alunos.isel.pt>
2025-02-22 16:53:35 +0000

    fix(link): changed the setters and getters to use lombok

--- src/main/java/consensus/core/Message.java
+++ src/main/java/consensus/core/Message.java
@@ -8,8 +8,9 @@ import java.io.Serializable;
 @Getter
 public class Message implements Serializable {
     private final int senderId;
+    @Setter
     private int messageId;
-    private Type type;
+    private final Type type;
 
     public enum Type {
         ACK, WRITE, RECEIVE
@@ -19,25 +20,4 @@ public class Message implements Serializable {
         this.senderId = senderId;
         this.type = type;
     }
-
-    public int getSenderId() {
-        return senderId;
-    }
-
-    public int getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(int messageId) {
-        this.messageId = messageId;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
 }
